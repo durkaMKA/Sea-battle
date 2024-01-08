f09bce2ef3ce39ccb3066c79d2c72364f25a0943
durkaMKA <miroslav050966@gmail.com>
2024-01-08 21:22:42 +0300

    Маленькое изменение

--- tester1.py
+++ tester1.py
@@ -2,65 +2,64 @@ from tkinter import *
 import time
 import random
 windows = Tk()
-windows.geometry('1000x360')
+windows.geometry('1980x1200')
 windows.title('Морской бой')
 
-#Противник
-def butb():
-    windowst = Tk()
-    windowst.geometry('1000x360')
-    windowst.title('Морской бой:противник')
-    buttonsl = [Button(windowst,width=7,height=4,font=('Times New Roman',9, 'bold'),command= lambda  x = i:Player2(x))for i in range(50)]
-    nazv = Label(windowst,text='Ваш противник',font=('Times New Roman',16))
-    nazv.place(rely=0.2, relx=0.7)
-    buttonsw.destroy()
-    
-    def Player2(bot):
-        
-        gamelo= [None]*50
-        gameb =list(range(50))
-       
-        gameb[bot]='X'
-        buttonsl[bot].config(text='X',state= 'disabled')
-        gamelo.remove(bot)
-        if gameb[bot]!= gameb[t]:
-            buttons[bot].config(text='X',state= 'disabled')
-        
-        t=random.randint(gamelo)
-        gameb[t]=' '
-        buttonsl[t].config(text=' ')
-    ow = 1
-    ol = 0
-    for i in range(50):
-        buttonsl[i].grid(row=ow,column=ol)
-        ol += 1
-        if ol ==10:
-            ow +=1
-            ol=0
-   
-#Пользователь
-gamel= [None]*50
+
 game =list(range(50))
-
-def Player1(w):
-    global turn
-    global gamel
+def BOT():
     global game
+    buttonsl = [Button(windows,width=7,height=4,font=('Times New Roman',9, 'bold'),command=lambda x = i: Player2(x))for i in range(50)]
+    row = 1
+    col = 12
+    pust = Label(windows,text='          ')
+    pust.grid(row=1,column=11)
+    for i in range(50):
+        buttonsl[i].grid(row=row,column=col)
+        col += 1
+        if col ==22:
+            row +=1
+            col=12
+   
     
-    game[w]='■'
-    buttons[w].config(text='■',state='disabled')
-    gamel.remove(w)
-    if w == range(50):
-        t = random.choice(gamel)
-        game[t]='X'
+    nazv2 = Label(windows,text=':ваш противник',font=('Times New Roman',16))
+    nazv2.grid(row=3,column=53)
+    
+    def Player2(l):
+        
+        global game
+        v = random.randint(0,50)
+        game[v]='X'
+        buttons[v].config(text='X',state='disabled')
+        
+        tt = random.randrange(0,50)
+        t = random.randrange(0,50)
+        game[l]='X'
+        game[tt]='  '
+        game[t]=' '
+       
+        for  k in range(t):
+            buttonsl[k].config(text=' ')
+            if  game[l] != game[t]:
+                buttonsl[l].config(text='X',state= 'disabled')
+        for kk in range(tt):
+            buttonsl[kk].config(text='  ')
+            if game[l] != game[tt]:
+                buttonsl[l].config(text='-',state='disabled')
         time.sleep(0.5)
-        buttons[t].config(text='X',state='disabled')
-
-nazv1 = Label(windows,text=' ')
-nazv1.place(rely=0.3,relx=0.7)
+        
+def Player1(w):
+   
+    global game
+    game[w]= '■'
+    buttons[w].config(text='■',state='disabled')
+    
+nazv1 = Label(windows,text='          ')
+nazv1.grid(row=2,column=11)
 buttons = [Button(windows,width=7,height=4,font=('Times New Roman',9, 'bold'),command= lambda  x = i:Player1(x))for i in range(50)]
-buttonsw = Button(windows,text='Начать игру',width=10,height=5,command=butb)
-buttonsw.place(rely=0.5,relx=0.7)
+
+buttonsw = Button(windows,text='Начать игру',width=20,height=5,command=BOT)
+buttonsw.place(relx=0.5,rely=0.7)
 
 
 
